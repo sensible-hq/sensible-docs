233b5ea30e90a2b24a9d688eb5721680f7f3ae81
frances elliott <fsc.elliott@gmail.com>
2021-08-20 15:56:53 -0600

    updates

--- make_docs.sh
+++ make_docs.sh
@@ -1,8 +1,7 @@
 direnv allow .
 echo "updating from Github"
 git pull
-echo "applying drop shadows to new images"
-# find all files in this dir maxdpeth 1, I believe exec opens the currently found file(?) add white border, clone last image in image sequence, add black background and shadow, switch the last 2 images in the sequence,  white background, merge (flatten) image layers, remove the layer offset (+repage),  
-find ./readme-sync/assets/v0/images/ -maxdepth 1 -type f -exec convert {} -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage borders/{} \;
+echo "applying drop shadows to new images" 
+find ./readme-sync/assets/v0/images/ -maxdepth 1 -type f -execdir convert {} -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage borders/{} \;
 echo "syncing to Readme "
-npx ts-node sync/index.ts --apiKey $README_API_KEY --version v0 --docs ~/Github/sensible-docs/readme-sync/v0
+npx ts-node ~/Github/readme-sync/sync/index.ts --apiKey $README_API_KEY --version v0 --docs ~/Github/sensible-docs/readme-sync/v0
