14a43ce5ce4850b08f79edc55b724d67839100b3
frances elliott <fsc.elliott@gmail.com>
2021-08-23 10:38:50 -0600

    automating adding dropshadow

--- make_docs.sh
+++ make_docs.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
 direnv allow .
 echo "updating from Github"
 git pull
-echo "applying drop shadows to new images" 
-find ./readme-sync/assets/v0/images/ -maxdepth 1 -type f -execdir convert {} -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage borders/{} \;
+echo "applying drop shadows to any new images"
+# for any PNG that exists in ./readme-sync/assets/v0/images/ but not ./readme-sync/assets/v0/images/borders, convert to drop shadow
+# and write to borders dir
+for file in ./readme-sync/assets/v0/images/*.png
+do
+  # regex replacment: ${baseString/patternToMatch/replacePatternWithThis}
+  borderFile="${file/images/images/borders}"
+  if [ -f "$file" ] && [ ! -f "$borderFile" ]
+  then
+    echo "processing $file and writing to $borderFile" 
+    convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$borderFile"
+  fi
+done
 echo "syncing to Readme "
 npx ts-node ~/Github/readme-sync/sync/index.ts --apiKey $README_API_KEY --version v0 --docs ~/Github/sensible-docs/readme-sync/v0
