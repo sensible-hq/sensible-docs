7d163f6d6e45696216deaad847b5da53540c912e
frances elliott <fsc.elliott@gmail.com>
2021-09-09 12:22:40 -0600

    updating local style changes to images

--- make_docs.sh
+++ make_docs.sh
@@ -2,7 +2,7 @@
 direnv allow .
 echo "updating from Github"
 git pull
-echo "applying drop shadows to screenshots"
+echo "applying drop shadows to screenshots if any were recently committed"
 # for any PNG that exists in ./readme-sync/assets/v0/images/ but not ./readme-sync/assets/v0/images/final, convert to drop shadow
 # and write to final dir
 
@@ -12,11 +12,14 @@ for file in ./readme-sync/assets/v0/images/screenshots/*.png
 do
   # regex replacment: ${baseString/patternToMatch/replacePatternWithThis}
   finalFile="${file/images\/screenshots/images\/final}"
+  lastCommit=$(git log -n 1 --date=relative --format=%cd $file)
   #if [ -f "$file" ] && [ ! -f "$finalFile" ]
-  #then
+  # if the image was committed in the last 2 days, update it. this should catch all updates as long as you sync docs soon after modifying images
+  if [[ "$lastCommit" =~ .*+(second|minute|hour).* ]] 
+  then
   echo "processing $file and writing to $finalFile" 
   convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
-  #fi
+  fi
 done
 
 # if there are local uncommited changes, commit them (for example as output of imagemagick)
