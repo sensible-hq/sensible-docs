3542cbcd0660f01a024be24085c6f7c3c94c8dba
frances elliott <fsc.elliott@gmail.com>
2021-09-09 11:10:54 -0600

    screenshots

--- make_docs.sh
+++ make_docs.sh
@@ -2,7 +2,7 @@
 direnv allow .
 echo "updating from Github"
 git pull
-echo "applying drop shadows to any new filenames in screenshots but not in final"
+echo "applying drop shadows to screenshots"
 # for any PNG that exists in ./readme-sync/assets/v0/images/ but not ./readme-sync/assets/v0/images/final, convert to drop shadow
 # and write to final dir
 
@@ -12,11 +12,11 @@ for file in ./readme-sync/assets/v0/images/screenshots/*.png
 do
   # regex replacment: ${baseString/patternToMatch/replacePatternWithThis}
   finalFile="${file/images\/screenshots/images\/final}"
-  if [ -f "$file" ] && [ ! -f "$finalFile" ]
-  then
-    echo "processing $file and writing to $finalFile" 
-    convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
-  fi
+  #if [ -f "$file" ] && [ ! -f "$finalFile" ]
+  #then
+  echo "processing $file and writing to $finalFile" 
+  convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
+  #fi
 done
 
 # if there are local uncommited changes, commit them (for example as output of imagemagick)
