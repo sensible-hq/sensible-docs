7bdc4d2a402633a412dd459df83d65a033e923cf
frances elliott <fsc.elliott@gmail.com>
2021-09-09 14:14:32 -0600

    column images

--- make_docs.sh
+++ make_docs.sh
@@ -17,7 +17,7 @@ do
   # if the image was committed in the last 2 days, update it. this should catch all updates as long as you sync docs soon after modifying images
   if [[ "$lastCommit" =~ .*+(second|minute|hour).* ]] 
   then
-  echo "processing $file and writing to $finalFile because it was last committed $lastCommit" 
+  echo "updating $finalFile because its source was last committed $lastCommit" 
   convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
   fi
 done
@@ -27,8 +27,11 @@ done
 echo "syncing to Readme "
 npx ts-node ~/Github/readme-sync/sync/index.ts --apiKey $README_API_KEY --version v0 --docs ~/Github/sensible-docs/readme-sync/v0
 
-
-# if there are local uncommitted changes, commit them (for example as output of imagemagick)
+#if there are local uncommitted changes, commit them for example as output of imagemagick
 if ! git diff-index --quiet HEAD --; then
+    echo "******************************************"
+    echo "******************************************"
     echo "REMEMBER TO COMMIT LOCAL UNTRACKED CHANGES"
+    echo "******************************************"
+    echo "******************************************"
 fi
