a8e7e46983c8f61a5b37e88ac2459020ab942baa
frances elliott <fsc.elliott@gmail.com>
2021-09-17 14:09:04 -0600

    updating local style changes to images

--- make_docs.sh
+++ make_docs.sh
@@ -15,9 +15,10 @@ do
   lastCommit=$(git log -n 1 --date=relative --format=%cd $file)
   # if [ -f "$file" ] && [ ! -f "$finalFile" ] 
   # any commits in the last 24 hrs (upper limit? unsure. less than 47 hrs?), OR if file exists in screenshots but not final dir
-  if [[ "$lastCommit" =~ .*+(second|minute|hour).* ]] || ([ -f "$file" ] && [ ! -f "$finalFile" ])
+  #if [[ "$lastCommit" =~ .*+(second|minute|hour).* ]] || ([ -f "$file" ] && [ ! -f "$finalFile" ])
+  if [[ "$lastCommit" =~ .*+(second|minute).* ]] || ([ -f "$file" ] && [ ! -f "$finalFile" ])
   then
-  echo "updating $finalFile because its source was last committed $lastCommit" 
+  echo "updating $finalFile because it doesn't exist in /final or its source was last committed $lastCommit" 
   # apply a drop shadow to screenshots
   convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
   fi
