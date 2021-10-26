10faf1e77090345764e7d8621753281635d43220
frances elliott <fsc.elliott@gmail.com>
2021-10-26 09:52:21 -0600

    paths

--- scripts/check_links/check_links.rb
+++ scripts/check_links/check_links.rb
@@ -2,25 +2,26 @@ require 'html-proofer'
 require 'html/pipeline'
 require 'find'
 
+puts "current dir:"
 puts Dir.pwd
 
-# make an out dir
-Dir.mkdir("out") unless File.exist?("out")
+# # make an out dir
+# Dir.mkdir("out") unless File.exist?("out")
 
-pipeline = HTML::Pipeline.new [
-  HTML::Pipeline::MarkdownFilter,
-  HTML::Pipeline::TableOfContentsFilter
-], :gfm => true
+# pipeline = HTML::Pipeline.new [
+#   HTML::Pipeline::MarkdownFilter,
+#   HTML::Pipeline::TableOfContentsFilter
+# ], :gfm => true
 
-# iterate over files, and generate HTML from Markdown
-Find.find("/readme-sync/v0/") do |path|
-  if File.extname(path) == ".md"
-    contents = File.read(path)
-    result = pipeline.call(contents)
+# # iterate over files, and generate HTML from Markdown
+# Find.find("/readme-sync/v0/") do |path|
+#   if File.extname(path) == ".md"
+#     contents = File.read(path)
+#     result = pipeline.call(contents)
 
-    File.open("out/#{path.split("/").pop.sub('.md', '.html')}", 'w') { |file| file.write(result[:output].to_s) }
-  end
-end
+#     File.open("out/#{path.split("/").pop.sub('.md', '.html')}", 'w') { |file| file.write(result[:output].to_s) }
+#   end
+# end
 
-# test your out dir's links!
-HTMLProofer.check_directory("./out").run
+# # test your out dir's links!
+# HTMLProofer.check_directory("./out").run
