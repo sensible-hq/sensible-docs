fd3a828a3a4464e250ec0582ebdf248b6129250b
frances elliott <fsc.elliott@gmail.com>
2021-11-23 12:50:51 -0700

    scripting

--- scripts/check_links/get_readme_changelog.py
+++ scripts/check_links/get_readme_changelog.py
@@ -26,18 +26,19 @@ def get_changelogs():
     rel_path = "./out"
     if not os.path.exists(rel_path):
       os.makedirs(rel_path, 0o755)
+    file_path = os.path.join(rel_path, "all_changelogs, ".", html")  
     print("PATHS: current:", os.getcwd())
-    print("PATHS: intended dest:", abs_file_path)
-    if not os.path.exists(abs_file_path):
+    print("PATHS: intended dest:", file_path)
+    if not os.path.exists(file_path):
         print("dest doesn't exist!")
     # left off TODO: make an out dir?
     for page in response_json:
       #print(json.dumps(page['html'], indent=2))
       # left off: write each html to some ./out dir, same as the ruby one...
-      with open(os.path.join(rel_path, "all_changelogs, ".", html"), 'a+') as f:
+      with open(file_path, 'a+') as f:
         f.write(page['html'])
     print("ALL CHANGELOGS")
-    with open(abs_file_path, 'r') as fin:
+    with open(file_path, 'r') as fin:
       print(fin.read())
 
 def get_doc_slugs(cat_id):
