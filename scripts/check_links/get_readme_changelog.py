c954959e59bedb44e409917c75a79a5c8993f52e
frances elliott <fsc.elliott@gmail.com>
2021-11-23 12:35:32 -0700

    scripting

--- scripts/check_links/get_readme_changelog.py
+++ scripts/check_links/get_readme_changelog.py
@@ -23,7 +23,7 @@ def get_changelogs():
     #print(json.dumps(response_json, indent=2))
 
     script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-    rel_path = "./out/all_changelogs.html"
+    rel_path = "./scripts/out/all_changelogs.html"
     if not os.path.exists(rel_path):
       os.makedirs(rel_path, 0o755)
     abs_file_path = os.path.join(script_dir, rel_path)
