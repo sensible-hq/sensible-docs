a42f87d3aa02d68abec1a2927b094204dcdc6be6
frances elliott <fsc.elliott@gmail.com>
2022-09-06 15:18:40 -0600

    force udpate pics

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# you must run this inside the scripts dir not from the parent dir. todo - fix this requirement so can run from root dir
 direnv allow .
 echo "updating from Github"
 git pull
@@ -15,12 +17,11 @@ do
   # regex replacment: ${baseString/patternToMatch/replacePatternWithThis}
   finalFile="${file/images\/screenshots/images\/final}"
   lastCommit=$(git log -n 1 --date=relative --format=%cd $file)
-  # if [ -f "$file" ] && [ ! -f "$finalFile" ] 
   # any commits in the last 24 hrs (upper limit? unsure. less than 47 hrs?), OR if file exists in screenshots but not final dir
 
   if [[ "$lastCommit" =~ .*+(second|minute).* ]] || ([ -f "$file" ] && [ ! -f "$finalFile" ])
   then
-  echo "updating $finalFile because it doesn't exist in /final or its source was last committed $lastCommit" 
+  echo "updating $finalFile because it doesn't exist in /final or its source was last committed $lastCommit"
   # apply a drop shadow to screenshots
   convert "$file" -bordercolor white -border 0 \( +clone -background black -shadow 80x3+2+2 \) +swap -background white -layers merge +repage "$finalFile"
   fi
@@ -38,7 +39,7 @@ npx ts-node ~/Github/readme-sync/sync/index.ts --apiKey $ENVRC_README_API_KEY --
 git ls-files --others --error-unmatch . >/dev/null 2>&1; ec=$?
 if test "$ec" = 0; then
     echo some untracked files
-    git add -A 
+    git add -A
     echo "git status:"
     git status
     git commit -m "updating local style changes to images"
@@ -51,7 +52,7 @@ fi
 
 if ! git diff-index --quiet HEAD --; then
     echo "local staged changes exist"
-fi    
+fi
 
 
 
@@ -59,11 +60,9 @@ fi
 rm logs.csv
 touch logs.csv
 
-# npx linkinator https://docs.sensible.so/changelog --verbosity ERROR --recurse --format CSV >> logs.csv 
-# npx linkinator https://docs.sensible.so/reference --verbosity ERROR --recurse --format CSV >> logs.csv 
+# npx linkinator https://docs.sensible.so/changelog --verbosity ERROR --recurse --format CSV >> logs.csv
+# npx linkinator https://docs.sensible.so/reference --verbosity ERROR --recurse --format CSV >> logs.csv
 
-npx linkinator https://docs.sensible.so/docs --verbosity ERROR --recurse --format CSV >> logs.csv 
+npx linkinator https://docs.sensible.so/docs --verbosity ERROR --recurse --format CSV >> logs.csv
 
 cat logs.csv
-
-
