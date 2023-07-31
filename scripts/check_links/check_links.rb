eb5d06d6a94d1d67999ae8cb945b2e1b30ed7a39
fscelliott <42477011+fscelliott@users.noreply.github.com>
2023-07-31 14:31:32 -0600

    docs toolchain

--- scripts/check_links/check_links.rb
+++ scripts/check_links/check_links.rb
@@ -99,16 +99,16 @@ options = {
 
 # check the guides
 
-# \(ref\:
-# make sure that  in the guides, any "internal" '(ref:' links syntax is resolved to full URLs (note: '(doc:' syntax links auto-checked by readme-sync tool anyway, no need to resolve those)
 
+# make sure that  in the guides, any "internal" '(ref:' links syntax is resolved to full URLs (note: '(doc:' syntax links auto-checked by readme-sync tool anyway, no need to resolve those)
+# not working currently:
 # puts Dir.pwd
 # puts Pathname.new(".").children.select { |c| c.directory? }
 # puts Pathname.new(".out").children.select { |c| c.directory? }
 # Dir.each_child("./out") do |file_name|
 #   puts file_name
 #   text = File.read(file_name)
-#   new_contents = text.gsub(/test1/, "https://docs.sensible.so/reference/")
+#   new_contents = text.gsub(/\(ref\:/, "https://docs.sensible.so/reference/")
 #   puts new_contents
 #   File.open(file_name, "w") {|file| file.puts new_contents }
 # end
