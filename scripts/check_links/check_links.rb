540c733053ce1fc57174629ff908bbcadc12a943
fscelliott <42477011+fscelliott@users.noreply.github.com>
2024-08-19 11:42:38 -0600

    Update check_links.rb

--- scripts/check_links/check_links.rb
+++ scripts/check_links/check_links.rb
@@ -88,11 +88,13 @@ end
 
 # #################
 # test your out dir's links!
+# ignore internal links like doc:color
 # #################
 
 options = {
   :log_level => :info,
-  :url_ignore => ["https://help.openai.com/en/articles/6654000-best-practices-for-prompt-engineering-with-openai-api","https://platform.openai.com/tokenizer","https://platform.openai.com/docs/guides/prompt-engineering","https://beta.openai.com/docs/"]
+  :url_ignore => ["https://help.openai.com/en/articles/6654000-best-practices-for-prompt-engineering-with-openai-api","https://platform.openai.com/tokenizer","https://platform.openai.com/docs/guides/prompt-engineering","https://beta.openai.com/docs/"],
+  :external_only
 }
 
 # puts "contents of Out:"
