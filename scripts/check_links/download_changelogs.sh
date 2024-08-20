5bd6de9bbeb1503757320d1e5c45e02b3c61b929
fscelliott <42477011+fscelliott@users.noreply.github.com>
2024-08-20 11:42:37 -0600

    Update download_changelogs.sh

--- scripts/check_links/download_changelogs.sh
+++ scripts/check_links/download_changelogs.sh
@@ -19,9 +19,10 @@ if [ $? -ne 0 ] || [ -z "$response" ]; then
   exit 1
 fi
 
-# Remove relative links from the response and convert to absolute URL links
-# This time, we handle the JSON properly, without converting it to a string
-response_json=$(echo "$response" | jq 'map_values(gsub("\\(doc:"; "(https://docs.sensible.so/docs/")) | map_values(gsub("\\(ref:"; "(https://docs.sensible.so/reference/")) | map_values(gsub("\\(changelog:"; "(https://docs.sensible.so/changelog/"))')
+# Replace relative links in the 'html' field within each 'page' object
+response_json=$(echo "$response" | jq '.[].html |= gsub("\\(doc:"; "(https://docs.sensible.so/docs/") |
+                                              .html |= gsub("\\(ref:"; "(https://docs.sensible.so/reference/") |
+                                              .html |= gsub("\\(changelog:"; "(https://docs.sensible.so/changelog/")')
 
 # Create the output directory
 rel_path="out_changelogs"
@@ -29,7 +30,7 @@ rm -rf "$rel_path"
 mkdir "$rel_path"
 
 # Loop through the response and save each page as a file
-echo "$response_json" | jq -c '.[]' | while read -r page; do
+echo "$response" | jq -c '.[]' | while read -r page; do
   slug=$(echo "$page" | jq -r '.slug')
   html=$(echo "$page" | jq -r '.html')
   file_path="$rel_path/$slug.html"
